d49f2ba64c6a2754643083c3da88a9c029092218
wacwac <wacogne.hugo@gmail.com>
2024-05-22 10:44:00 +0200

    page 404

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -1,7 +1,18 @@
+import Header from "../components/header";
+import HeaderModal from "../components/headerModal";
+
 export default function Error404 (){
 
     return(
-        <h1>erreur</h1>
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+            <main>
+                <h2>Page introuvable</h2>
+
+            </main>
+        </div>
 
     );
 }
